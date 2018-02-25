9e42efb5f8b1f1f55cfdc90680f4c847fae4f572
Sam Guliker <samguliker@hotmail.com>
2018-02-25 08:41:26 +0100

    Add @ Senmetsu

---
+++ 29 - Countdown Timer/scripts-START.js
@@ -0,0 +1,52 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('display__end-time')
+const buttons = document.querySelectorAll('[data-time]');
+
+function timer(seconds){
+    // clear any exsiting timers
+    clearInterval(countdown)
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds)
+
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        // check if we should stop it
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+
+        //display it
+        displayTimeLeft(secondsLeft)
+    },1000);
+}
+
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    document.title = display;
+    console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp){
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  }
+  
+  buttons.forEach(button => button.addEventListener('click', startTimer));
+  document.customForm.addEventListener('submit',function(e){
+      e.preventDefault();
+      const mins = this.minutes.value
+      timer(mins*60);
+      this.reset();
+  })
